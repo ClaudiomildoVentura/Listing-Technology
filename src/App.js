f279e0311b831fb3dfd915238c231893982ffd68
Claudiomildo <claudiomildo@gmail.com>
2019-10-20 19:35:38 -0300

    initialized with create react app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import './styles.css'
+import Routes from './routes'
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <Routes />
+  </div>
+)
+export default App
